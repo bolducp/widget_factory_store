87f4b0dd7a89ac25c3478b894e5270e44a3b22d4
Paige Bolduc <paigebolduc@gmail.com>
2016-11-06 18:13:17 -0500

    Troubleshoot gh-pages deploy.

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,8 +1,9 @@
 (function (global) {
   System.config({
+    baseURL: '/widget_factory_store/',
     paths: {
       // paths serve as alias
-      'npm:': 'node_modules/'
+      'npm:': 'https://unpkg.com/'
     },
     // map tells the System loader where to look for things
     map: {
